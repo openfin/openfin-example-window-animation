bec7f31cc43bb86d860a9928259cf8803384ee4e
mrClapham <info@gcdesign.co.uk>
2016-01-21 16:20:26 +0000

    Added Karma/Jasmine for testing.

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,19 @@
-/**
- * Created by grahamclapham on 21/01/16.
- */
-document.addEventListener("DOMContentLoaded", function(){
-   init();
-});
-
-var init = function init(){
+function init(){
     console.log("Dom Loaded ", this);
+    try{
+        fin.desktop.main(function(){
+            initWithOpenFin();
+        })
+    }catch(err){
+        initNoOpenFin();
+    }
 };
+
+function initWithOpenFin(){
+    alert("OpenFin is available");
+    // Your OpenFin specific code to go here...
+}
+
+function initNoOpenFin(){
+    alert("OpenFin is not available - you are probably running in a browser.");
+}
---
+++ test/index-test.js
@@ -0,0 +1,5 @@
+describe("A suite of tests for your OpenFin app", function(){
+    it("Should say true is true", function(){
+        expect(true).toEqual(true);
+    });
+});
