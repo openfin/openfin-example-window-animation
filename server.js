bf7e231df0583c0fca1ebca4c2313d93d4825692
mrClapham <info@gcdesign.co.uk>
2016-01-21 09:29:19 +0000

    Initial commit.

---
+++ server.js
@@ -0,0 +1,20 @@
+var   express = require('express')
+    , http = require('http')
+    , path = require('path');
+
+var app = express();
+
+app.set('title','OpenFin appseed test');
+app.use(express.static(path.join(__dirname, 'src')));
+
+/* serves main page  */
+app.get('/', function (req, res) {
+    res.sendFile("src/index.html", {"root": __dirname});
+});
+
+/* process.env.PORT is used in case you want to push to Heroku, for example, here the port will be dynamically allocated */
+var port = process.env.PORT || 9070;
+
+http.createServer(app).listen(port, function(){
+    console.log('Express server listening on port ' + port);
+});
